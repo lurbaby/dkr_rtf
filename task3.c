258fb58af2dac5084721eb8f50bf69b9edb3538d
lurbaby <olexandrchoriy@gmail.com>
2023-12-18 21:17:51 +0200

    done all 3 task

---
+++ task3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <locale.h>
+#include "string.h"
+#include <ctype.h>
+
+
+int main() {
+
+    setlocale(LC_ALL, "ukr");
+
+    char input;
+    char str[1024] = "432423rwef23fwe234";
+
+    printf("Введіть текст (завершіть введення, натиснувши Ctrl+Z):\n");
+
+
+    while ((input = getchar() ) != EOF){
+        putchar(input);
+        strcat(str,&input);
+    }
+
+    int i = 0;
+    int result = 0;
+
+    while (str[i] != '\0'){
+        if (isdigit(str[i]))
+            result++;
+        i++;
+    }
+
+    printf("знайдено %d цифр", result);
+
+}
